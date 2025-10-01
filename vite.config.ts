f8f7face15725849b377533d907e2908218ce367
Aleksandr Edemskii <a7edemskiy@gmail.com>
2025-10-01 21:34:26 +0300

    set versel deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/travel-expenses',
+  // base: '/travel-expenses',
+  base: './',
   plugins: [react()],
 });
